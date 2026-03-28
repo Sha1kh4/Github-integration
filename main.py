3c05b3a20b35d88fb8459a6150766565a3427012
sha1kh4 <adnan.shaikh166@mit.asia>
2026-03-28 14:14:57 +0530

    Get access token from github

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 import os
 from dotenv import load_dotenv
 from starlette.responses import RedirectResponse
+import httpx
 
 load_dotenv()
 
@@ -14,9 +15,12 @@ async def root():
 
 @app.get("/github")
 async def github_login(request: Request):
-    return RedirectResponse(url=f"https://github.com/login/oauth/authorize?client_id={os.getenv('Client_ID')}&redirect_uri={request.base_url}callback",status_code=302)
+    return RedirectResponse(url=f"https://github.com/login/oauth/authorize?client_id={os.getenv('Client_ID')}&redirect_uri={request.base_url}callback&scope=read:user",status_code=302)
 
 @app.get("/callback")
 async def github_callback(code: str):
-    return {"message": "GitHub token received", "code": code}
-    
+    url = f"https://github.com/login/oauth/access_token?client_id={os.getenv('Client_ID')}&client_secret={os.getenv('Client_secrets')}&code={code}"
+    headers = {"Accept": "application/json"}
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+    return {"message": "GitHub token received", "code": response.text}
