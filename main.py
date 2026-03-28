fa05fb5fa5ef698921b73bfb054a320ee3112d38
sha1kh4 <adnan.shaikh166@mit.asia>
2026-03-28 16:25:45 +0530

    add other required routes

--- main.py
+++ main.py
@@ -67,3 +67,79 @@ async def me(token: Annotated[str, Depends(oauth2_scheme)]):
 
     }
     return data
+
+@app.get("/create-issue")
+async def create_issue(token: Annotated[str, Depends(oauth2_scheme)], title: str, body: str,reponame: str,label : str = "bug"):
+    url = f"https://api.github.com/repos/{reponame}/issues"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2026-03-10"
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, headers=headers, json={"title": title, "body": body, "labels": [label]})
+    if response.status_code != 201:
+        return {"message": "Failed to create issue", "code": response.json()}
+    
+    data = {
+        "title": title,
+        "body": body,
+        "labels": label
+    }
+    return data,response.json()
+
+@app.get("/get-issues")
+async def get_issues(token: Annotated[str, Depends(oauth2_scheme)], reponame: str):
+    url = f"https://api.github.com/repos/{reponame}/issues"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2026-03-10"
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+    if response.status_code != 200:
+        return {"message": "Failed to get issues", "code": response.json()}
+
+    return response.json()
+
+@app.get("/get-commits")
+async def get_commits(token: Annotated[str, Depends(oauth2_scheme)], reponame: str):
+    url = f"https://api.github.com/repos/{reponame}/commits"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2026-03-10"
+    }
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+    if response.status_code != 200:
+        return {"message": "Failed to get commits", "code": response.json()}
+    
+    return response.json()
+
+@app.get("/crete-pull-request")
+async def crete_pull_request(token: Annotated[str, Depends(oauth2_scheme)], title: str, body: str,reponame: str,branch : str ,base : str = "main"):
+    url = f"https://api.github.com/repos/{reponame}/pulls"
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2026-03-10"
+    }
+    print(f"https://api.github.com/repos/{reponame}/pulls")
+    print(headers)
+    print(title,body,branch,base)
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, headers=headers, json={"title": title, "body": body, "head": f"{branch}", "base": base})
+    if response.status_code != 201:
+        raise HTTPException(
+            status_code=response.status_code,
+            detail={
+                "message": "Failed to create pull request",
+                "error": response.json()
+            }
+        )
+    return response.json()
